d710c52756140d4cf8f6aafd2b16da4219eefbfd
Minor Gordon <minorg@server.fake>
2023-08-16 10:04:24 -0400

    dev.sh: only output data for now

--- dev.sh
+++ dev.sh
@@ -9,18 +9,19 @@ CACHE_DIR_PATH=$THIS_DIR_PATH/.paradicms/cache
 DATA_DIR_PATH=$THIS_DIR_PATH/.paradicms/data
 PROJECTS_DIR_PATH=$PWD/..
 
-cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
+cd $PROJECTS_DIR_PATH/paradicms/lib/py/nlp
 
 poetry run $PROJECTS_DIR_PATH/paradicms-spreadsheet-etl-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
+  --enrichers "pdf,text" \
   --force-extract \
   --loaded-data-directory-path $DATA_DIR_PATH \
   --pipeline-id brunswick-ny-us \
   --spreadsheet "1sxHX7qNYheEle2E-FRYV24UUTLEfoE4eXTxGwx1XSWY" \
   "$@"
 
-poetry run $PROJECTS_DIR_PATH/paradicms-ssg-action/action.py \
-  --cache-directory-path $CACHE_DIR_PATH \
-  --data-path $DATA_DIR_PATH \
-  --next-commands dev \
-  --pipeline-id brunswick-ny-us
+# poetry run $PROJECTS_DIR_PATH/paradicms-ssg-action/action.py \
+#   --cache-directory-path $CACHE_DIR_PATH \
+#   --data-path $DATA_DIR_PATH \
+#   --next-commands dev \
+#   --pipeline-id brunswick-ny-us
